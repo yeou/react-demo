6ee7cca89a047863fcc710a5a6a86e94bac3bbce
Yeager Vanhooren <yeager.vanhooren@gmail.com>
2017-09-20 23:53:13 +0200

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
+import todoReducer from './todo/reducers';
+
+const store = createStore(todoReducer);
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
 });
